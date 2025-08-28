cd35405157775cfc61c19d9fc0fa404e9352928a
Walidhasan2003 <mdwalidhasan4217252003@gmail.com>
2025-08-28 13:34:21 +0600

    added a heart count number

---
+++ emergency-hotline/emergency.js
@@ -0,0 +1,89 @@
+// heart-1//
+const emergency1 = document.getElementById('heart-1')
+.addEventListener('click', function(){
+   const newValue = document.getElementById('love')
+   const valueNew = parseInt(newValue.innerText)
+   const newNumber = valueNew + 1
+   newValue.innerText = newNumber
+   
+})
+// heart-2//
+const emergency2 = document.getElementById('heart-2')
+.addEventListener('click', function(){
+   const newValue = document.getElementById('love')
+   const valueNew = parseInt(newValue.innerText)
+   const newNumber = valueNew + 1
+   newValue.innerText = newNumber
+   
+})
+// heart-3/
+const emergency3 = document.getElementById('heart-3')
+.addEventListener('click', function(){
+   const newValue = document.getElementById('love')
+   const valueNew = parseInt(newValue.innerText)
+   const newNumber = valueNew + 1
+   newValue.innerText = newNumber
+   
+})
+// heart-4//
+const emergency4 = document.getElementById('heart-4')
+.addEventListener('click', function(){
+   const newValue = document.getElementById('love')
+   const valueNew = parseInt(newValue.innerText)
+   const newNumber = valueNew + 1
+   newValue.innerText = newNumber
+   
+})
+// heart-5//
+const emergency5 = document.getElementById('heart-5')
+.addEventListener('click', function(){
+   const newValue = document.getElementById('love')
+   const valueNew = parseInt(newValue.innerText)
+   const newNumber = valueNew + 1
+   newValue.innerText = newNumber
+   
+})
+// heart-6//
+const emergency6 = document.getElementById('heart-6')
+.addEventListener('click', function(){
+   const newValue = document.getElementById('love')
+   const valueNew = parseInt(newValue.innerText)
+   const newNumber = valueNew + 1
+   newValue.innerText = newNumber
+   
+})
+// heart-7//
+const emergency7 = document.getElementById('heart-7')
+.addEventListener('click', function(){
+   const newValue = document.getElementById('love')
+   const valueNew = parseInt(newValue.innerText)
+   const newNumber = valueNew + 1
+   newValue.innerText = newNumber
+})
+// heart-8//
+const emergency8 = document.getElementById('heart-8')
+.addEventListener('click', function(){
+   const newValue = document.getElementById('love')
+   const valueNew = parseInt(newValue.innerText)
+   const newNumber = valueNew + 1
+   newValue.innerText = newNumber
+   
+})
+// heart-9//
+const emergency9 = document.getElementById('heart-9')
+.addEventListener('click', function(){
+   const newValue = document.getElementById('love')
+   const valueNew = parseInt(newValue.innerText)
+   const newNumber = valueNew + 1
+   newValue.innerText = newNumber
+   
+})
+
+
+document.getElementById('amar').addEventListener('click', function(){
+   const newNumber = document.getElementById('value')
+   const addValue = parseInt(newNumber.innerText)
+   const addNewValue = addValue + 1;
+   newNumber.innerText = addNewValue
+})
+
