3ab25bb1d46e35b3da879242f3822abf7f7fb745
Walidhasan2003 <mdwalidhasan4217252003@gmail.com>
2025-08-29 23:40:02 +0600

    added javascript

--- emergency-hotline/emergency.js
+++ emergency-hotline/emergency.js
@@ -1,3 +1,4 @@
+// heartNumber strts here//
 // heart-1//
 const emergency1 = document.getElementById('heart-1')
 .addEventListener('click', function(){
@@ -78,12 +79,588 @@ const emergency9 = document.getElementById('heart-9')
    newValue.innerText = newNumber
    
 })
+// heartNumber starts ends here//
+
+// copyNumber starts here//
+
+// copy-1//
+ const newCopyValue1 = document.getElementById('copy1').addEventListener('click', function() {
+   const newCopyNumber = document.getElementById('copy')
+   const addNewCopyNumber = parseInt(newCopyNumber.innerText)
+   const addCopy = addNewCopyNumber + 1
+   newCopyNumber.innerText = addCopy
 
 
-document.getElementById('amar').addEventListener('click', function(){
-   const newNumber = document.getElementById('value')
-   const addValue = parseInt(newNumber.innerText)
-   const addNewValue = addValue + 1;
-   newNumber.innerText = addNewValue
+   navigator.clipboard.writeText(addCopy).then(() => {
+      alert("The number has been copied: 999.... ")
+   }).catch(err => {
+      alert("Failed to copy: " + err)
+   })
 })
+
+
+// copy-2//
+ const newCopyValue2 = document.getElementById('copy2').addEventListener('click', function() {
+   const newCopyNumber = document.getElementById('copy')
+   const addNewCopyNumber = parseInt(newCopyNumber.innerText)
+   const addCopy = addNewCopyNumber + 1
+   newCopyNumber.innerText = addCopy
+
+   navigator.clipboard.writeText(addCopy).then(() => {
+      alert("The number has been copied: 999.... ")
+   }).catch(err => {
+      alert("Failed to copy: " + err)
+   })
+})
+// copy-3//
+const newCopyValue3 = document.getElementById('copy3').addEventListener('click',function(){
+   const newCopyNumber = document.getElementById('copy')
+   const addNewCopyNumber = parseInt(newCopyNumber.innerText)
+   const addCopy = addNewCopyNumber + 1
+   newCopyNumber.innerText = addCopy
+
+   navigator.clipboard.writeText(addCopy).then(() => {
+      alert("The number has been copied: 999.... ")
+   }).catch(err => {
+      alert("Failed to copy: " + err)
+   })
+
+})
+// copy-4//
+const newCopyValue4 = document.getElementById('copy4').addEventListener('click',function(){
+   const newCopyNumber = document.getElementById('copy')
+   const addNewCopyNumber = parseInt(newCopyNumber.innerText)
+   const addCopy = addNewCopyNumber + 1
+   newCopyNumber.innerText = addCopy
+
+   navigator.clipboard.writeText(addCopy).then(() => {
+      alert("The number has been copied: 1994-9999999.... ")
+   }).catch(err => {
+      alert("Failed to copy: " + err)
+   })
+
+
+})
+// copy-5//
+const newCopyValue5 = document.getElementById('copy5').addEventListener('click',function(){
+   const newCopyNumber = document.getElementById('copy')
+   const addNewCopyNumber = parseInt(newCopyNumber.innerText)
+   const addCopy = addNewCopyNumber + 1
+   newCopyNumber.innerText = addCopy
+
+   navigator.clipboard.writeText(addCopy).then(() => {
+      alert("The number has been copied: 109.... ")
+   }).catch(err => {
+      alert("Failed to copy: " + err)
+   })
+})
+// copy6//
+const newCopyValue6 = document.getElementById('copy6').addEventListener('click',function(){
+   const newCopyNumber = document.getElementById('copy')
+   const addNewCopyNumber = parseInt(newCopyNumber.innerText)
+   const addCopy = addNewCopyNumber + 1
+   newCopyNumber.innerText = addCopy
+
+   navigator.clipboard.writeText(addCopy).then(() => {
+      alert("The number has been copied: 106.... ")
+   }).catch(err => {
+      alert("Failed to copy: " + err)
+   })
+})
+// copy//
+const newCopyValue7 = document.getElementById('copy7').addEventListener('click',function(){
+   const newCopyNumber = document.getElementById('copy')
+   const addNewCopyNumber = parseInt(newCopyNumber.innerText)
+   const addCopy = addNewCopyNumber + 1
+   newCopyNumber.innerText = addCopy
+
+   navigator.clipboard.writeText(addCopy).then(() => {
+      alert("The number has been copied: 16216.... ")
+   }).catch(err => {
+      alert("Failed to copy: " + err)
+   })
+
+})
+// copy8//
+const newCopyValue8 = document.getElementById('copy8').addEventListener('click',function(){
+   const newCopyNumber = document.getElementById('copy')
+   const addNewCopyNumber = parseInt(newCopyNumber.innerText)
+   const addCopy = addNewCopyNumber + 1
+   newCopyNumber.innerText = addCopy
+
+   navigator.clipboard.writeText(addCopy).then(() => {
+      alert("The number has been copied: 16445.... ")
+   }).catch(err => {
+      alert("Failed to copy: " + err)
+   })
+
+})
+// copy9//
+const newCopyValue9 = document.getElementById('copy9').addEventListener('click',function(){
+   const newCopyNumber = document.getElementById('copy')
+   const addNewCopyNumber = parseInt(newCopyNumber.innerText)
+   const addCopy = addNewCopyNumber + 1
+   newCopyNumber.innerText = addCopy
+
+
+
+   navigator.clipboard.writeText(addCopy).then(() => {
+      alert("The number has been copied: 163.... ")
+   }).catch(err => {
+      alert("Failed to copy: " + err)
+   })
+})
+// copy Number ends here//
+
+// callButton starts here//
+// call-1//
+const callNumber1 = document.getElementById('call1')
+.addEventListener('click',function(){
+   const newCallNumber = document.getElementById('coin')
+   const newCallValue = parseInt(newCallNumber.innerText)
+  
+   if(newCallValue >= 20){
+      const newValueNumber = newCallValue - 20;
+      newCallNumber.innerText = newValueNumber
+   }
+   else{
+      alert("দুঃখিত! আপনার পর্যাপ্ত কয়েন নেই। কল করতে অন্তত ২০ কয়েন প্রয়োজন।")
+   }
+
+
+
+})
+
+
+
+
+
+
+const transitionData1 = []
+    const data1 = {
+   name: 'National Emergency Number',
+   date: new Date().toLocaleString()
+ }
+ transitionData1.push(data1)
+ console.log(transitionData1)
+
+
+ document.getElementById('call1').addEventListener('click',function(){
+   const transitionCotiner = document.getElementById('callTransition')
+   for(const data of transitionData7){
+      const diElement = document.createElement('div')
+      diElement.innerHTML = `
+
+                        <div class="flex justify-between mt-5 px-1">
+                        <div class="flex items-center">
+                        <i class="fa-solid fa-phone"></i>
+                        <p>999</p>
+                       </div>
+                       <div>
+                        <h1>${data.name}</h1>
+                        <p>${data.date}</p>
+                       </div>
+                       </div>
+      `
+      transitionCotiner.appendChild(diElement)
+   }
+   alert('National Emergency Number999......')
+ })
+
+
+// call-2//
+const callNumber2 = document.getElementById('call2')
+.addEventListener('click',function(){
+   const newCallNumber = document.getElementById('coin')
+   const newCallValue = parseInt(newCallNumber.innerText)
+  
+   if(newCallValue >= 20){
+      const newValueNumber = newCallValue - 20;
+      newCallNumber.innerText = newValueNumber
+   }
+   else{
+      alert("দুঃখিত! আপনার পর্যাপ্ত কয়েন নেই। কল করতে অন্তত ২০ কয়েন প্রয়োজন।")
+   }
+})
+
+const transitionData2 = []
+    const data2 = {
+   name: 'Police Helpline Number',
+   date: new Date().toLocaleString()
+ }
+ transitionData2.push(data2)
+ console.log(transitionData2)
+
+
+ document.getElementById('call2').addEventListener('click',function(){
+   const transitionCotiner = document.getElementById('callTransition')
+   for(const data of transitionData7){
+      const diElement = document.createElement('div')
+      diElement.innerHTML = `
+
+                        <div class="flex justify-between mt-5 px-1">
+                        <div class="flex items-center">
+                        <i class="fa-solid fa-phone"></i>
+                        <p>999</p>
+                       </div>
+                       <div>
+                        <h1>${data.name}</h1>
+                        <p>${data.date}</p>
+                       </div>
+                       </div>
+      `
+      transitionCotiner.appendChild(diElement)
+   }
+   alert('Police Helpline Number999......')
+ })
+
+// // call-3//
+
+const callNumber3 = document.getElementById('call3')
+.addEventListener('click',function(){
+   const newCallNumber = document.getElementById('coin')
+   const newCallValue = parseInt(newCallNumber.innerText)
+  
+   if(newCallValue >= 20){
+      const newValueNumber = newCallValue - 20;
+      newCallNumber.innerText = newValueNumber
+   }
+   else{
+      alert("দুঃখিত! আপনার পর্যাপ্ত কয়েন নেই। কল করতে অন্তত ২০ কয়েন প্রয়োজন।")
+   }
+
+})
+
+
+const transitionData3 = []
+    const data3 = {
+   name: 'Fire Service Number',
+   date: new Date().toLocaleString()
+ }
+ transitionData3.push(data3)
+ console.log(transitionData3)
+
+
+ document.getElementById('call3').addEventListener('click',function(){
+   const transitionCotiner = document.getElementById('callTransition')
+   for(const data of transitionData7){
+      const diElement = document.createElement('div')
+      diElement.innerHTML = `
+
+                        <div class="flex justify-between mt-5 px-1">
+                        <div class="flex items-center">
+                        <i class="fa-solid fa-phone"></i>
+                        <p>999</p>
+                       </div>
+                       <div>
+                        <h1>${data.name}</h1>
+                        <p>${data.date}</p>
+                       </div>
+                       </div>
+      `
+      transitionCotiner.appendChild(diElement)
+   }
+   alert('Fire Service Number999......')
+ })
+
+// // call-4//
+const callNumber4 = document.getElementById('call4')
+.addEventListener('click',function(){
+   const newCallNumber = document.getElementById('coin')
+   const newCallValue = parseInt(newCallNumber.innerText)
+  
+   if(newCallValue >= 20){
+      const newValueNumber = newCallValue - 20;
+      newCallNumber.innerText = newValueNumber
+   }
+   else{
+      alert("দুঃখিত! আপনার পর্যাপ্ত কয়েন নেই। কল করতে অন্তত ২০ কয়েন প্রয়োজন।")
+   }
+
+})
+
+const transitionData4 = []
+    const data4 = {
+   name: 'Ambulance Service',
+   date: new Date().toLocaleString()
+ }
+ transitionData4.push(data4)
+ console.log(transitionData4)
+
+
+ document.getElementById('call4').addEventListener('click',function(){
+   const transitionCotiner = document.getElementById('callTransition')
+   for(const data of transitionData7){
+      const diElement = document.createElement('div')
+      diElement.innerHTML = `
+
+                        <div class="flex justify-between mt-5 px-1">
+                        <div class="flex items-center">
+                        <i class="fa-solid fa-phone"></i>
+                        <p>1994-999999</p>
+                       </div>
+                       <div>
+                        <h1>${data.name}</h1>
+                        <p>${data.date}</p>
+                       </div>
+                       </div>
+      `
+      transitionCotiner.appendChild(diElement)
+   }
+   alert('Ambulance Service1994-999999......')
+ })
+
+
+// // call-5//
+const callNumber5 = document.getElementById('call5')
+.addEventListener('click',function(){
+   const newCallNumber = document.getElementById('coin')
+   const newCallValue = parseInt(newCallNumber.innerText)
+  
+   if(newCallValue >= 20){
+      const newValueNumber = newCallValue - 20;
+      newCallNumber.innerText = newValueNumber
+   }
+   else{
+      alert("দুঃখিত! আপনার পর্যাপ্ত কয়েন নেই। কল করতে অন্তত ২০ কয়েন প্রয়োজন।")
+   }
+
+})
+
+ const transitionData5 = []
+    const data5 = {
+   name: 'Women & Child Helpline',
+   date: new Date().toLocaleString()
+ }
+ transitionData5.push(data5)
+ console.log(transitionData5)
+
+
+ document.getElementById('call5').addEventListener('click',function(){
+   const transitionCotiner = document.getElementById('callTransition')
+   for(const data of transitionData7){
+      const diElement = document.createElement('div')
+      diElement.innerHTML = `
+
+                        <div class="flex justify-between mt-5 px-1">
+                        <div class="flex items-center">
+                        <i class="fa-solid fa-phone"></i>
+                        <p>109</p>
+                       </div>
+                       <div>
+                        <h1>${data.name}</h1>
+                        <p>${data.date}</p>
+                       </div>
+                       </div>
+      `
+      transitionCotiner.appendChild(diElement)
+   }
+   alert('Women & Child Helpline 109......')
+ })
+
+
+
+// // call-6//
+const callNumber6 = document.getElementById('call6')
+.addEventListener('click',function(){
+   const newCallNumber = document.getElementById('coin')
+   const newCallValue = parseInt(newCallNumber.innerText)
+  
+   if(newCallValue >= 20){
+      const newValueNumber = newCallValue - 20;
+      newCallNumber.innerText = newValueNumber
+   }
+   else{
+      alert("দুঃখিত! আপনার পর্যাপ্ত কয়েন নেই। কল করতে অন্তত ২০ কয়েন প্রয়োজন।")
+   }
+
+})
+
+
+ const transitionData6 = []
+    const data6 = {
+   name: 'Anti-Corruption Helpline',
+   date: new Date().toLocaleString()
+ }
+ transitionData6.push(data6)
+ console.log(transitionData6)
+
+
+ document.getElementById('call6').addEventListener('click',function(){
+   const transitionCotiner = document.getElementById('callTransition')
+   for(const data of transitionData7){
+      const diElement = document.createElement('div')
+      diElement.innerHTML = `
+
+                        <div class="flex justify-between mt-5 px-1">
+                        <div class="flex items-center">
+                        <i class="fa-solid fa-phone"></i>
+                        <p>106</p>
+                       </div>
+                       <div>
+                        <h1>${data.name}</h1>
+                        <p>${data.date}</p>
+                       </div>
+                       </div>
+      `
+      transitionCotiner.appendChild(diElement)
+   }
+   alert('Anti-Corruption Helpline 106......')
+ })
+
+
+
+// // call-7//
+const callNumber7 = document.getElementById('call7')
+.addEventListener('click',function(){
+   const newCallNumber = document.getElementById('coin')
+   const newCallValue = parseInt(newCallNumber.innerText)
+  
+   if(newCallValue >= 20){
+      const newValueNumber = newCallValue - 20;
+      newCallNumber.innerText = newValueNumber
+   }
+   else{
+      alert("দুঃখিত! আপনার পর্যাপ্ত কয়েন নেই। কল করতে অন্তত ২০ কয়েন প্রয়োজন।")
+   }
+
+})
+
+ const transitionData7 = []
+    const data7 = {
+   name: 'Electricity Helpline',
+   date: new Date().toLocaleString()
+ }
+ transitionData7.push(data7)
+ console.log(transitionData7)
+
+
+ document.getElementById('call7').addEventListener('click',function(){
+   const transitionCotiner = document.getElementById('callTransition')
+   for(const data of transitionData7){
+      const diElement = document.createElement('div')
+      diElement.innerHTML = `
+
+                        <div class="flex justify-between mt-5 px-1">
+                        <div class="flex items-center">
+                        <i class="fa-solid fa-phone"></i>
+                        <p>16216</p>
+                       </div>
+                       <div>
+                        <h1>${data.name}</h1>
+                        <p>${data.date}</p>
+                       </div>
+                       </div>
+      `
+      transitionCotiner.appendChild(diElement)
+   }
+   alert('Electricity Helpline.16216......')
+ })
+
+// // call-8//
+const callNumber8 = document.getElementById('call8')
+.addEventListener('click',function(){
+   const newCallNumber = document.getElementById('coin')
+   const newCallValue = parseInt(newCallNumber.innerText)
+  
+   if(newCallValue >= 20){
+      const newValueNumber = newCallValue - 20;
+      newCallNumber.innerText = newValueNumber
+   }
+   else{
+      alert("দুঃখিত! আপনার পর্যাপ্ত কয়েন নেই। কল করতে অন্তত ২০ কয়েন প্রয়োজন।")
+   }
+
+ })
+
+ const transitionData8 = []
+    const data = {
+   name: 'Brac Helpline',
+   date: new Date().toLocaleString()
+ }
+ transitionData8.push(data)
+ console.log(transitionData8)
+
+
+ document.getElementById('call8').addEventListener('click',function(){
+   const transitionCotiner = document.getElementById('callTransition')
+   for(const data of transitionData8){
+      const diElement = document.createElement('div')
+      diElement.innerHTML = `
+
+                        <div class="flex justify-between mt-5 px-1">
+                        <div class="flex items-center">
+                        <i class="fa-solid fa-phone"></i>
+                        <p>16445</p>
+                       </div>
+                       <div>
+                        <h1>${data.name}</h1>
+                        <p>${data.date}</p>
+                       </div>
+                       </div>
+      `
+      transitionCotiner.appendChild(diElement)
+   }
+   alert('Brac Helpline16445....')
+ })
+
+
+
+// call9//
+ const callNumber = document.getElementById('call9')
+.addEventListener('click',function(){
+   const newCallNumber = document.getElementById('coin')
+   const newCallValue = parseInt(newCallNumber.innerText)
+  
+   if(newCallValue >= 20){
+      const newValueNumber = newCallValue - 20;
+      newCallNumber.innerText = newValueNumber
+   }
+   else{
+      alert("দুঃখিত! আপনার পর্যাপ্ত কয়েন নেই। কল করতে অন্তত ২০ কয়েন প্রয়োজন।")
+   }
+
+ })
+
+ const transitionData = []
+ const data9 = {
+   name: 'Bangladesh Railway Helpline',
+   date: new Date().toLocaleString()
+ }
+ transitionData.push(data9)
+ console.log(transitionData)
+
+
+ document.getElementById('call9').addEventListener('click',function(){
+   const transitionCotiner = document.getElementById('callTransition')
+   for(const data of transitionData){
+      
+      const diElement = document.createElement('div')
+      diElement.innerHTML = `
+
+                        <div class="flex justify-between mt-5 px-1">
+                        <div class="flex items-center">
+                        <i class="fa-solid fa-phone"></i>
+                        <p>163</p>
+                       </div>
+                       <div>
+                        <h1>${data.name}</h1>
+                        <p>${data.date}</p>
+                       </div>
+                       </div>
+      `
+      transitionCotiner.appendChild(diElement)
+   }
+   alert('Bangladesh Railway Helpline163....')
+ })
+
+
+
+ const clearAll = document.getElementById('remove')
+ .addEventListener('click',function(){
+   const remove = document.getElementById('callTransition')
+   remove.innerHTML = '';
+ })
+
+
 
